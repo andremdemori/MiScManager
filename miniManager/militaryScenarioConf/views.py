c6785b5d3dca3dd6303a508c3ec4129df550842a
demori <andredemori@ime.eb.br>
2023-05-20 16:32:42 -0300

    conclusão da impotação do cenário via owl

--- miniManager/militaryScenarioConf/views.py
+++ miniManager/militaryScenarioConf/views.py
@@ -131,11 +131,12 @@ class UploadScenarioView(TemplateView):
 
         #variáveis para armazenar os elementos do owl
         scenario_name = ''
-        latest_scenario = ''
+        scenario = ''
         MO_dictionary = {}
         commander_name = ''
         MP_dictionary = {}
-        guaranis = []
+        guaranis_dictionary = {}
+        guarani_om = ''
 
         CommDevices = []
         Operators = []
@@ -238,70 +239,80 @@ class UploadScenarioView(TemplateView):
 
                 if classe_ == 'Guarani':
                     for j in ind.instances():
-                        guaranis.append(j.name)
-
-
+                        if j.name not in seen_names:
+                            guaranis_dictionary[j.name] = {}
+                            seen_names.add(j.name)
+                            for p in j.get_properties():
+                                p_value = str(p._get_value_for_individual(j)).split(".")[-1]
+                                if p.name == 'belongsTo':
+                                    guaranis_dictionary[j.name][p.name] = p_value
 
             ### COMEÇA A POPULAR O BANCO DE DADOS COM OS DADOS IMPORTADOS ###
-            for i in range(len(list(ref_onto.classes()))):
-                classe = list(ref_onto.classes())[i]
+            #for i in range(len(list(ref_onto.classes()))):
+            #    classe = list(ref_onto.classes())[i]
 
-                if classe.name == 'MilitaryScenario':
-                    militaryscenario = str(j.name)
+            if 'MilitaryScenario' in [classe.name for classe in ref_onto.classes()]:
+                #militaryscenario = str(j.name)
 
-                    ###CRIA CENÁRIO ###
-                    #MilitaryScenario.objects.create(name=militaryscenario, description='')
-                    #scenario = MilitaryScenario.objects.get(name=militaryscenario) # nomes de cenários precisam ser únicos
+                ###CRIA CENÁRIO ###
 
-                if classe.name == 'MilitaryOrganization':
-                    om_name = ''
-                    om_type = ''
-                    commander = ''
-                    for key, value in MO_dictionary.items():
-                        print(f"\nKey: {key}")
-                        om_name = key
-                        for prop, prop_value in value.items():
-                            if prop == 'hasMOPowerType':
-                                om_type = prop_value
-                            if prop == 'isSubordinateTo':
-                                commander_name = prop_value
-                        if commander_name == '':
-                            None
-                            #MilitaryOrganization.objects.create(type=om_type,name=om_name,commander=None,scenario=latest_scenario)
-                        else:
-                            x=1
-                            #commander = MilitaryOrganization.objects.get(name=commander_name,scenario=scenario)
-                            #MilitaryOrganization.objects.create(type=om_type,name=om_name,commander=commander,scenario=latest_scenario)
+                scenario_id = MilitaryScenario.objects.latest('Id').Id + 1
+                MilitaryScenario.objects.create(Id=scenario_id, name=scenario_name, description='')
+                scenario = MilitaryScenario.objects.get(name=scenario_name) # nomes de cenários precisam ser únicos
 
-                if classe.name == 'Guarani':
-                    x=1
-                    for g in guaranis:
-                        x=1
-                        #Guarani.objects.create(name=g,scenario=scenario,VisibilityRange=1000,v_min=95,v_max=3.5,category='Armored')
+            if 'MilitaryOrganization' in [classe.name for classe in ref_onto.classes()]:
+                om_name = ''
+                om_type = ''
+                commander = ''
+                for key, value in MO_dictionary.items():
+                    print(f"\nKey: {key}")
+                    om_name = key
+                    commander_name == ''
+                    for prop, prop_value in value.items():
+                        if prop == 'hasMOPowerType':
+                            om_type = prop_value
+                            om_type = MilitaryOrganizationPowerType.objects.get(name=om_type)
+                        if prop == 'isSubordinateTo':
+                            commander_name = prop_value
+                    if commander_name == '':
+                        om_id = MilitaryOrganization.objects.latest('Id').Id + 1
+                        MilitaryOrganization.objects.create(Id=om_id,type=om_type,name=om_name,commander=None,scenario=scenario)
+                    else:
+                        commander = MilitaryOrganization.objects.get(name=commander_name,scenario=scenario)
+                        om_id = MilitaryOrganization.objects.latest('Id').Id + 1
+                        MilitaryOrganization.objects.create(Id=om_id,type=om_type,name=om_name,commander=commander,scenario=scenario)
 
-                if classe.name == 'MilitaryPerson':
-                    x=1
-                    identifier = ''
-                    vehicle = ''
-                    by_foot = False
-                    for key, value in MP_dictionary.items():
-                        print(f"\nKey: {key}")
-                        identifier = key
-                        for prop, prop_value in value.items():
-                            if prop == 'militaryPersonHasMilitaryOrganization':
-                                mo_om = prop_value
-                                #mo_om = MilitaryOrganization.objects.get(name=mo_om,scenario=scenario)
-                            if prop == 'isLocatedIn':
-                                vehicle = prop_value
-                                carrier = Guarani.objects.get(name=vehicle)
-                            if prop == 'carries':
-                                by_foot = True
-                                #carrier = Carrier.objects.get(Id=1) # by foot
+            if 'Guarani' in [classe.name for classe in ref_onto.classes()]:
+                for key, value in guaranis_dictionary.items():
+                    #print(f"\nKey: {key}")
+                    guarani_name = key
+                    guarani_om == ''
+                    for prop, prop_value in value.items():
+                        if prop == 'belongsTo':
+                            guarani_om = prop_value
+                            guarani_om = MilitaryOrganization.objects.get(name=guarani_om,scenario=scenario)
+                    g_id = Guarani.objects.latest('Id').Id + 1
+                    Guarani.objects.create(Id=g_id,name=guarani_name,scenario=scenario,VisibilityRange=1000,v_min=95,v_max=3.5,category='Armored', Military_Organization=guarani_om)
 
+            if 'MilitaryPerson' in [classe.name for classe in ref_onto.classes()]:
+                identifier = ''
+                vehicle = ''
+                by_foot = False
+                for key, value in MP_dictionary.items():
+                    #print(f"\nKey: {key}")
+                    identifier = key
+                    for prop, prop_value in value.items():
+                        if prop == 'militaryPersonHasMilitaryOrganization':
+                            mo_om = prop_value
+                            mo_om = MilitaryOrganization.objects.get(name=mo_om,scenario=scenario)
+                        if prop == 'isLocatedIn':
+                            vehicle = prop_value
+                            carrier = Guarani.objects.get(name=vehicle, scenario=scenario)
+                        if prop == 'carries':
+                            by_foot = True
+                            carrier = Carrier.objects.get(Id=1) # by foot
 
-                        #MilitaryPerson.objects.create(Identifier=identifier,Military_Organization=mo_om,CommDevice_Carrier=carrier,scenario=scenario)
-
-
+                    MilitaryPerson.objects.create(Identifier=identifier,Military_Organization=mo_om,CommDevice_Carrier=carrier,scenario=scenario)
 
         context = {
             "types": types,
